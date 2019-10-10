2433831e5eb4da56481d18cfaca9cdb704d8115c
Blayne Chard <blayne@chard.com>
2019-10-10 20:05:17 +1300

    feat: test index.js

--- .hyperfine.js
+++ .hyperfine.js
@@ -1,4 +1,7 @@
 module.exports = [{
     name: 'Sleep 100ms',
     command: 'sleep 0.1'
+}, {
+    name: 'Test index',
+    command: 'node index.js'
 }]
---
+++ index.js
@@ -0,0 +1,6 @@
+async function main() {
+    await new Promise(resolve => setTimeout(resolve, 200))
+}
+
+
+main()
